2c847c9a9410d9a5d76d16dc4f3de076190d5936
Cecilia Clohisy <cclohisy@gmail.com>
2018-03-31 16:26:41 -0500

    logging movie and song data

--- liri.js
+++ liri.js
@@ -25,86 +25,204 @@ for (var i = 3; i < inputArray.length; i++) {
         input += inputArray[i];
     }
 }
-console.log(input)
 
-// * `my-tweets`
-if (command == "my-tweets") {
+function myTweets() {
     client.get("statuses/user_timeline", params, function (error, tweets, response) {
         if (!error) {
             var tweetArray = tweets
             console.log("\n----- My Last 20 Tweets -----")
-            for (var i = 0; i < tweetArray.length; i++){               
-                console.log("Tweet " + (i+1) +": " + tweets[i].text + "\n Created on " + tweets[i].created_at)
+            for (var i = 0; i < tweetArray.length; i++) {
+                console.log("Tweet " + (i + 1) + ": " + tweets[i].text + "\n Created on " + tweets[i].created_at)
+
             }
+            console.log("\n")
         }
     })
-}//closes tweet command
-
-// * `spotify-this-song`
-if (command == "spotify-this-song") {
-    // https://api.spotify.com/v1/search?q=abba&type=track
-    spotify.search({
-        type: 'track',
-        query: input,
-        limit: 1
-    }, function(err, response){
-
-        if (err) {
-            console.log("---------------------")
-            console.error('Something went wrong', err.message);
-            return;
-        }
-        else {
-            console.log("----- Song Information -----")
-            console.log("Song title: " + response.tracks.items[0].name)
-            console.log("Album: " + response.tracks.items[0].album.name)
-            console.log("Artist: " + response.tracks.items[0].artists[0].name)
-            console.log(response.tracks.items[0].id)
-            // A preview link of the song from Spotify
-        }
-
-})
 }
 
-
-
-
-
-// * `movie-this`
-if (command == "movie-this") {
+function movieSearch() {
     var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
 
     request(queryUrl, function (error, response, body) {
-        console.log(JSON.parse(body).Response)
         // If the request was successful
         if (JSON.parse(body).Response == "True") {
-
-            console.log(JSON.parse(body).Title)
-            console.log("Release in " + JSON.parse(body).Year)
-            console.log("Film was rated " + JSON.parse(body).Rated)
-            console.log("Film was made in " + JSON.parse(body).Country)
+            console.log("\n-----Movie Information-----")
+            console.log("Movie: " + JSON.parse(body).Title)
+            console.log("Rating: " + JSON.parse(body).Rated)
+            console.log("Released in " + JSON.parse(body).Year)
+            console.log("Language: " + JSON.parse(body).Language)
+            console.log("Filmed in " + JSON.parse(body).Country)
             console.log("Plot: " + JSON.parse(body).Plot)
             console.log("Actors: " + JSON.parse(body).Actors)
 
             var ratings = JSON.parse(body).Ratings
-            console.log("IMDB rating " + ratings[0].Value)
-            console.log(ratings[1].Source + " rating " + ratings[1].Value)
+            if (ratings[0] == undefined) {
 
-        }
+            }
+            else { console.log("IMDB rating " + ratings[0].Value) }
+            //debugger;
+            if (ratings[1] == undefined) {
+
+            }
+            else { console.log(ratings[1].Source + " rating " + ratings[1].Value) }
+            fs.appendFile("log.txt",
+                "\nMovie: " + JSON.parse(body).Title + ", " + "Rating: " + JSON.parse(body).Rated + ", " + "Released in " + JSON.parse(body).Year + ", "
+                + "Language: " + JSON.parse(body).Language + ", " + "Filmed in " + JSON.parse(body).Country + ", " + "Plot: " + JSON.parse(body).Plot + ", " +
+                "Actors: " + JSON.parse(body).Actors + ", " + "IMDB rating " + ratings[0].Value + ", " + ratings[1].Source + " rating " + ratings[1].Value,
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("\nMovie data successfully logged!")
+                        console.log("")
+                    }
+                })
+
+        }//closes results
+
         //else Mr. Nobody outputs
         else {
-            console.log("Mr. Nobody")
+            var queryUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
+            request(queryUrl, function (error, response, body) {
+                console.log("\n-------No information found for that movie-------")
+                console.log("\nYou should watch Mr. Nobody!")
+                console.log("\nMovie: " + JSON.parse(body).Title)
+                console.log("Released in " + JSON.parse(body).Year)
+                console.log("Film is rated " + JSON.parse(body).Rated)
+                console.log("Film was made in " + JSON.parse(body).Country)
+                console.log("Plot: " + JSON.parse(body).Plot)
+                console.log("Actors: " + JSON.parse(body).Actors)
 
-        }
+                var ratings = JSON.parse(body).Ratings
+                console.log("IMDB rating " + ratings[0].Value)
+                console.log(ratings[1].Source + " rating " + ratings[1].Value)
+
+            })
+        }//closes mr nobody
     })//closes request    
-}//closes movie if statement 
-
-// * `do-what-it-says`
-if (command == "do-what-it-says") {
-    // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-
-    // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-    // Feel free to change the text in that document to test out the feature for other commands.
 
 }
+function songSearch() {
+    spotify.search({
+        type: 'track',
+        query: input,
+        limit: 1
+    }, function (err, response) {
+
+        if (err) {
+            console.error('\nSomething went wrong', err.message);
+            spotify.request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+                .then(function (sign) {
+                    //debugger;
+                    console.log("\n----- Song was not found -----")
+                    console.log("Check out this song from the Swedish pop sensation Ace of Base instead!")
+                    console.log("\nSong title: " + sign.name)
+                    console.log("Album: " + sign.album.name)
+                    console.log("Artist: " + sign.album.artists[0].name)
+                    console.log("Preview Song: " + sign.preview_url)
+                    console.log("")
+                }).catch(function (err) {
+                    console.error('Error occurred: ' + err);
+                })
+            return;
+        }
+        else {
+            console.log("")
+            console.log("----- Song Information -----")
+            console.log("Song title: " + response.tracks.items[0].name)
+            console.log("Album: " + response.tracks.items[0].album.name)
+            console.log("Artist: " + response.tracks.items[0].artists[0].name)
+            if (response.tracks.items[0].preview_url == null) {
+                console.log("Preview Song: Preview not available for this song, sorry!")
+            }
+            else {
+                console.log("Preview Song: " + response.tracks.items[0].preview_url)
+            };
+            fs.appendFile("log.txt", "\nSong title: " + response.tracks.items[0].name + ", " + "Album: " + response.tracks.items[0].album.name + ", "
+                + "Artist: " + response.tracks.items[0].artists[0].name + ", " + "Preview Song: " + response.tracks.items[0].preview_url,
+                function (err) {
+                    if (err) {
+                        console.log("Data could not be logged" + err);
+                    }
+                    else {
+                        console.log("Song data successfully logged!")
+                        console.log("")
+                    }
+                })
+
+        }
+
+    })
+}
+// spotify.search({ type: 'track', query: input, limit: 1 }, function (err, response) {
+//     if (err) {
+//         spotify.request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+//             .then(function (sign) {
+//                 //debugger;
+//                 console.log("----- Song was not found -----")
+//                 console.log("Check out this song from the Swedish pop sensation Ace of Base instead!")
+//                 console.log("\nSong title: " + sign.name)
+//                 console.log("Album: " + sign.album.name)
+//                 console.log("Artist: " + sign.album.artists[0].name)
+//                 console.log("Preview Song: " + sign.preview_url)
+//             }).catch(function (err) {
+//                 console.error('Error occurred: ' + err);
+//             })
+//     }
+//     {
+//         console.log("\n----- Song Information -----")
+//         console.log("Song title: " + response.tracks.items[0].name)
+//         console.log("Album: " + response.tracks.items[0].album.name)
+//         console.log("Artist: " + response.tracks.items[0].artists[0].name)
+//         // console.log(response.tracks.items[0].id)
+//         if (response.tracks.items[0].preview_url == null) {
+//             console.log("Preview Song: Preview not available for this song, sorry!")
+//         }
+//         else {
+//             console.log("Preview Song: " + response.tracks.items[0].preview_url)
+//         };
+//         console.log("i got here")
+//         fs.appendFile("log.txt", "\nSong title: " + response.tracks.items[0].name + ", " + "Album: " + response.tracks.items[0].album.name + ", "
+//             + "Artist: " + sign.album.artists[0].name + ", " + "Preview Song: " + sign.preview_url,
+//             function (err) {
+//                 if (err) {
+//                     console.log(err);
+//                 }
+//                 else {
+//                     console.log("Song data successfully logged!")
+//                     console.log("")
+//                 }
+//             })
+//     }
+// })
+
+switch (command) {
+    // * `my-tweets`
+    case "my-tweets":
+        myTweets()
+        break
+
+    case "spotify-this-song":
+        songSearch()
+        break
+
+    // * `movie-this`
+    case "movie-this":
+        movieSearch()
+        break
+
+    // * `do-what-it-says`
+    case "do-what-it-says":
+        // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+        fs.readFile("random.txt", "utf8", function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(data)
+            }
+        })
+        break
+
+}//close switch
